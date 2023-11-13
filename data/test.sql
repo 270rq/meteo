4d033a6a8e1bb80fb8460b0f4c79d5fef4c7c7ba
Katya <chuprova-katya@list.ru>
2023-11-13 17:54:27 +0300

    some

--- data/test.sql
+++ data/test.sql
@@ -1,9 +1,9 @@
 
 INSERT INTO sun (date, city_id,sunset, sunrise)
-VALUES ('2023-11-09',411,'16:30:00', '08:30:00');
+VALUES ('2023-11-13',411,'16:30:00', '08:30:00');
 
 INSERT INTO menu (id, city_id, temperature, precipitation, uv_index, wind_speed, wind_direction, visibility)
-VALUES ('2023-11-09 18:24:00', 411, -5.0, 0.5, 2, 10.0, 'NE', 10.0);
+VALUES ('2023-11-13 18:24:00', 411, -5.0, 0.5, 2, 10.0, 'NE', 10.0);
 
 INSERT INTO family (name)
 VALUES ('Злаки'),
